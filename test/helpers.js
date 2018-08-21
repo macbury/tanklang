a0d5e0f91139b172a7162b133dd8b3641c17aa5d
Arkadiusz Buras <macbury@gmail.com>
2018-08-21 20:15:10 +0200

    WIP

--- test/helpers.js
+++ test/helpers.js
@@ -1,9 +1,23 @@
 import { VirtualMachine } from '../src/virtual_machine'
+import Compiler from '../src/compiler'
+import { readFileSync } from 'fs'
 
 export function withVM(instructions, callback) {
   return function(done) {
-    let vm = new VirtualMachine(instructions, )
+    let vm = new VirtualMachine(instructions)
     callback(vm)
+    done()
+  }
+}
+
+export function compile(path, callback) {
+  return function(done) {
+    let compiler = new Compiler()
+    let bytecode = compiler.compile(readFileSync(path))
+    let program = bytecode.toProgram()
+    let vm = new VirtualMachine(program)
+    vm.run()
+    callback(vm, bytecode)
     done()
   }
 }
