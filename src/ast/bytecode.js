adfda6fb8a7865e520a59580f28c2d23dab972fd
Arkadiusz Buras <macbury@gmail.com>
2018-08-22 15:29:57 +0200

    WIP

--- src/ast/bytecode.js
+++ src/ast/bytecode.js
@@ -1,14 +1,66 @@
 import Opcodes from '../opcodes'
 
+class Instruction {
+  constructor(opcodeName, operands) {
+    this.opcodeName = opcodeName
+    this._operands = operands
+  }
+
+  get operands() {
+    return this._operands
+  }
+
+  set operands(newOps) {
+    if (newOps.length == this._operands.length) {
+      this._operands = newOps
+    } else {
+      throw new Error(`Diffrent number of operands in instruction`)
+    }
+  }
+
+  get opcode() {
+    return Opcodes[this.opcodeName]
+  }
+
+  get length() {
+    return this._operands ? this._operands.length + 1 : 1
+  }
+
+  haveOperands() {
+    return this._operands != null && this._operands.length > 0
+  }
+
+  toJs() {
+    if (this.haveOperands()) {
+      return { opcode: this.opcodeName, operands: this._operands }
+    } else {
+      return { opcode: this.opcodeName }
+    }
+  }
+
+  toBytes(bytes) {
+    bytes.push(this.opcode)
+    if (this.haveOperands()) {
+      for (var j = 0; j < this._operands.length; j++) {
+        bytes.push(this.operands[j])
+      }
+    }
+  }
+}
+
 export default class Bytecode {
   program = []
+  address = 0
 
   push(opcodeName, ...operands) {
-    if (operands == null || operands.length == 0) {
-      this.program.push({ opcode: opcodeName })
-    } else {
-      this.program.push({ opcode: opcodeName, operands })
-    }
+    let instruction = new Instruction(opcodeName, operands)
+    this.program.push(instruction)
+    this.address += instruction.length
+    return this
+  }
+
+  get nextAddress() {
+    return this.address + 1
   }
 
   get(index) {
@@ -16,7 +68,7 @@ export default class Bytecode {
   }
 
   toArray() {
-    return this.program.slice()
+    return this.program.map((instruction) => instruction.toJs())
   }
 
   toBlob() {
@@ -26,13 +78,7 @@ export default class Bytecode {
   toProgram() {
     let code = []
     for (var i = 0; i < this.program.length; i++) {
-      let { opcode, operands } = this.program[i]
-      code.push(Opcodes[opcode])
-      if (operands != null) {
-        for (var j = 0; j < operands.length; j++) {
-          code.push(operands[j])
-        }
-      }
+      this.program[i].toBytes(code)
     }
     return code
   }
