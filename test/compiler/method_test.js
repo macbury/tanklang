eb443474c63a6e4d3b1b90b3957bbc737615c630
Arkadiusz Buras <macbury@gmail.com>
2018-08-24 08:24:15 +0200

    Define method

---
+++ test/compiler/method_test.js
@@ -0,0 +1,26 @@
+import { expect } from 'chai'
+import { loadAndcompile } from '../helpers'
+
+describe('methods', function () {
+  it.only('generating method', loadAndcompile('./test/factories/methods/simple.tank', function(vm, bytecode) {
+    expect(bytecode).to.deep.eq([
+      { opcode: 'Push', operands: [0] },// initialize a
+      { opcode: 'Store', operands: [1] }, 
+
+      { opcode: 'Push', operands: [1000] }, // set a to 1000
+      { opcode: 'Store', operands: [1] }, 
+
+      { opcode: 'Push', operands: [0] },// initialize b
+      { opcode: 'Store', operands: [2] }, 
+
+      // jmp after method
+      // method body start
+      // push argA
+      // push argB
+      // call method
+      
+      { opcode: 'Halt' }
+    ])
+    expect(vm.frame.get(1)).to.deep.eq(2)
+  }))
+})
