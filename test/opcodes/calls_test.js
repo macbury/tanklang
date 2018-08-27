269241698057be51dc8a4b865bbebfb8f946a794
Arkadiusz Buras <macbury@gmail.com>
2018-08-27 09:39:23 +0200

    WIP

--- test/opcodes/calls_test.js
+++ test/opcodes/calls_test.js
@@ -4,19 +4,19 @@ import Opcodes from '../../src/opcodes'
 
 describe('VirtualMachine', function() {
   describe('Call', function() {
-    it('it moves to new function', withVM([Opcodes.Call, 0], function(vm) {
-      vm.step()
+    it('it moves to new function', withVM([Opcodes.Call, 0], async function(vm) {
+      await vm.step()
       expect(vm.ip).to.eq(0)
       expect(vm.frames.length).to.eq(2)
       expect(vm.frame.returnAddress).to.eq(2)
     }))
 
-    it('requires address operand', withVM([Opcodes.Call], function(vm) {
-      expect(() => vm.step()).to.throw(/Should have the instruction address after the Call instruction/)
+    it('requires address operand', withVM([Opcodes.Call], async function(vm) {
+      await expect(vm.step()).be.rejectedWith(Error, /Should have the instruction address after the Call instruction/)
     }))
 
-    it('requires valid address', withVM([Opcodes.Call, 1000], function(vm) {
-      expect(() => vm.step()).to.throw(/Address 1000 is outside of program/)
+    it('requires valid address', withVM([Opcodes.Call, 1000], async function(vm) {
+      await expect(vm.step()).be.rejectedWith(Error, /Address 1000 is outside of program/)
     }))
 
     it('doubles values', withVM([
@@ -26,27 +26,27 @@ describe('VirtualMachine', function() {
       Opcodes.Push, 2, 
       Opcodes.Mul, 
       Opcodes.Ret
-    ], function(vm) {
-      vm.run()
+    ], async function(vm) {
+      await vm.run()
       expect(vm.stack.toArray()).to.deep.eq([6])
     }))
   })
 
   describe('Ret', function() {
-    it('it allows return to place of call', withVM([Opcodes.Call, 3, Opcodes.Halt, Opcodes.Ret], function(vm) {
-      expect(vm.step()).to.be.true
+    it('it allows return to place of call', withVM([Opcodes.Call, 3, Opcodes.Halt, Opcodes.Ret], async function(vm) {
+      expect(await(vm.step())).to.be.true
       expect(vm.frames.length).to.eq(2)
       expect(vm.ip).to.eq(3)
-      expect(vm.step()).to.be.true
+      expect(await(vm.step())).to.be.true
       expect(vm.ip).to.eq(2)
       expect(vm.frames.length).to.eq(1)
-      expect(vm.step()).to.be.false
+      expect(await(vm.step())).to.be.false
 
       expect(vm.ip).to.eq(3)
     }))
 
-    it('prevents return if no call has been triggered', withVM([Opcodes.Ret], function(vm) {
-      expect(() => vm.step()).to.throw(/Missing Call instruction/)
+    it('prevents return if no call has been triggered', withVM([Opcodes.Ret], async function(vm) {
+      await expect(vm.step()).be.rejectedWith(Error, /Missing Call instruction/)
     }))
   })
 })
--- test/opcodes/flow_test.js
+++ test/opcodes/flow_test.js
@@ -4,53 +4,53 @@ import Opcodes from '../../src/opcodes'
 
 describe('VirtualMachine', function() {
   describe('flow', function() {
-    it('Halt', withVM([Opcodes.Halt, Opcodes.Push], function(vm) {
-      expect(vm.step()).to.be.false
-      expect(() => vm.step()).to.throw(/VM is halted/)
+    it('Halt', withVM([Opcodes.Halt, Opcodes.Push], async function(vm) {
+      expect(await(vm.step())).to.be.false
+      await expect(vm.step()).be.rejectedWith(Error, /VM is halted!/)
       expect(vm.ip).to.eq(1)
       expect(vm.halted).to.be.true
       expect(vm.stack.toArray()).to.be.empty
     }))
 
     describe('Jmp', function() {
-      it('set ip to address', withVM([Opcodes.Push, 1, Opcodes.Jmp, 0], function(vm) {
-        vm.step()
+      it('set ip to address', withVM([Opcodes.Push, 1, Opcodes.Jmp, 0], async function(vm) {
+        await vm.step()
         expect(vm.ip).to.eq(2)
-        vm.step()
+        await vm.step()
         expect(vm.ip).to.eq(0)
       }))
 
-      it('throws error if trying to jump to something other than address', withVM([Opcodes.Jmp, 'sss'], function(vm) {
-        expect(() => vm.step()).to.throw(/Address sss is invalid/)
+      it('throws error if trying to jump to something other than address', withVM([Opcodes.Jmp, 'sss'], async function(vm) {
+        await expect(vm.step()).be.rejectedWith(Error, /Address sss is invalid/)
       }))
 
-      it('requires address operand', withVM([Opcodes.Jmp], function(vm) {
-        expect(() => vm.step()).to.throw(/Should have the instruction address after the Jmp instruction/)
+      it('requires address operand', withVM([Opcodes.Jmp], async function(vm) {
+        await expect(vm.step()).be.rejectedWith(Error, /Should have the instruction address after the Jmp instruction/)
       }))
     })
 
     describe('Jif', function() {
-      it('jump if true', withVM([Opcodes.Push, 1, Opcodes.Jif, 0], function(vm) {
-        vm.step()
+      it('jump if true', withVM([Opcodes.Push, 1, Opcodes.Jif, 0], async function(vm) {
+        await vm.step()
         expect(vm.ip).to.eq(2)
-        vm.step()
+        await vm.step()
         expect(vm.ip).to.eq(0)
         expect(vm.halted).to.be.false
       }))
 
-      it('dont jump if false', withVM([Opcodes.Push, 0, Opcodes.Jif, 0], function(vm) {
-        expect(vm.step()).to.be.true
-        expect(vm.step()).to.be.false
+      it('dont jump if false', withVM([Opcodes.Push, 0, Opcodes.Jif, 0], async function(vm) {
+        expect(await(vm.step())).to.be.true
+        expect(await(vm.step())).to.be.false
         expect(vm.halted).to.be.true
       }))
 
-      it('requires address operand', withVM([Opcodes.Jif], function(vm) {
+      it('requires address operand', withVM([Opcodes.Jif], async function(vm) {
         vm.stack.set([2])
-        expect(() => vm.step()).to.throw(/Should have the instruction address after the Jif instruction/)
+        await expect(vm.step()).be.rejectedWith(Error, /Should have the instruction address after the Jif instruction/)
       }))
 
-      it('requires value on stack', withVM([Opcodes.Jif], function(vm) {
-        expect(() => vm.step()).to.throw(/Stack underflow error/)
+      it('requires value on stack', withVM([Opcodes.Jif], async function(vm) {
+        await expect(vm.step()).be.rejectedWith(Error, /Stack underflow error/)
       }))
     })
   })
